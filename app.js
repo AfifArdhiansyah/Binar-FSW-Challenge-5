7de6a9e944cd21109b968e56598c8851a967887f
Afif Ardhiansyah <afifardhiansyah@gmail.com>
2022-10-04 17:23:36 +0700

    create view home

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use("/", indexRouter);
 app.use("/cars", carsRouter);
