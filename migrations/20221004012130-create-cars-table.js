7a4c62f36b40d4761f762d96c8b4414d85024d12
Afif Ardhiansyah <afifardhiansyah@gmail.com>
2022-10-04 08:29:19 +0700

    create db

---
+++ migrations/20221004012130-create-cars-table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('cars', {
+       id: {type: Sequelize.INTEGER , primaryKey: true, autoIncrement: true},
+       name: {type: Sequelize.STRING , allowNull: false},
+       year: {type: Sequelize.INTEGER , allowNull: false},
+       createdAt: {type: Sequelize.DATE , allowNull: false},
+       updatedAt: {type: Sequelize.DATE , allowNull: false}
+      });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('cars');
+  }
+};
