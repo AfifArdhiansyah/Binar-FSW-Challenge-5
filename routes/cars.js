6eed5ca224d2f8565b2eb4f2dabe534404dfd765
Afif Ardhiansyah <afifardhiansyah@gmail.com>
2022-10-04 14:37:59 +0700

    edit put method

--- routes/cars.js
+++ routes/cars.js
@@ -29,7 +29,6 @@ router.post('/', async (req, res) => {
     const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
     const allowedType = ['.png','.jpg','.jpeg'];
 
-    (req.body.year) ? req.body.year = parseInt(req.body.year) : req.body.year = null;
     const schema = {
         name: {type: "string", min: 3, max: 50},
         year: {type: "number", positive: true, integer: true, min: 1886, max: 2022},
@@ -60,16 +59,67 @@ router.post('/', async (req, res) => {
 
 //PUT
 router.put('/:id', async (req, res) => {
+    const car = await Cars.findByPk(req.params.id);
+    if(!car){
+        return res.status(404).json({msg: "No Data Found"});
+    }
+
+    let url;
+    let name;
+    let year;
+    let fileName;
+    let ext = '';
+    let fileSize;
+    let file;
+    let allowedType = ['.png','.jpg','.jpeg'];
+
+    if(req.files){
+        file = req.files.img;
+        fileSize = file.size
+        ext = path.extname(file.name);
+        fileName = file.md5 + ext;
+        url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    }
+    else{
+        url = car.img;
+    }
+
+    (req.body.name) ? name = req.body.name : name = car.name;
+    (req.body.year) ? year = parseInt(req.body.year) : year = car.year;
+        
     const schema = {
         name: {type: "string", min: 3, max: 50},
-        year: {type: "number", positive: true, integer: true, min: 1886, max: 2022}
+        year: {type: "number", positive: true, integer: true, min: 1886, max: 2022},
+        img: {type: "string", min: 3, max: 255}
     };
     const validate = v.compile(schema);
-    const valid = validate(req.body);
-    if (valid === true) {
-        const car = await Cars.update(req.body, {where: {id: req.params.id}});
-        res.json(car);
-    } else {
+    const valid = validate({name: name, year: year, img: url});
+
+    if(valid){
+        if(req.files){
+            if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
+            if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
+
+            file.mv(`./public/images/${fileName}`, async(err)=>{
+                if(err) return res.status(500).json({msg: err.message});
+                try {
+                    await Cars.update({name: name, year: year, img: url}, {where: {id: req.params.id}});
+                    res.status(201).json({msg: "Cars Updated Successfuly"});
+                } catch (error) {
+                    console.log(error.message);
+                }
+            })
+        }
+        else{
+            try {
+                await Cars.update({name: name, year: year, img: url}, {where: {id: req.params.id}});
+                res.status(201).json({msg: "Cars Updated Successfuly"});
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    }
+    else{
         res.status(400).json({error: valid});
     }
 });
