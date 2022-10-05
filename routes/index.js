11d42d6c4d236425c0b9eeeadf7e9ca6cd3dace9
Afif Ardhiansyah <afifardhiansyah@gmail.com>
2022-10-05 23:12:55 +0700

    create view add and update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
     res.render('pages/index', { title: 'Express' });
 });
 
+/* GET add page. */
+router.get('/add', function(req, res, next) {
+    res.render('pages/add-page', { title: 'Express' });
+});
+
+/* GET update page. */
+router.get('/update', function(req, res, next) {
+    res.render('pages/update-page', { title: 'Express' });
+});
+
 module.exports = router;
