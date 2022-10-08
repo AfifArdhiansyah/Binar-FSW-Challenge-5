c56164b950f82cdcf1c8d2b84e9af2a976cc210e
Afif Ardhiansyah <afifardhiansyah@gmail.com>
2022-10-08 07:36:02 +0700

    crud by view

--- routes/cars.js
+++ routes/cars.js
@@ -14,13 +14,16 @@ router.get('/', async (req, res) => {
 
 //GET ONE
 router.get('/:id', async (req, res) => {
-    const car = await Cars.findByPk(req.params.id);
-    res.json(car);
+    if(typeof req.params.id === 'number'){
+        const car = await Cars.findByPk(req.params.id);
+        res.json(car);
+    }
 });
 
 //POST
 router.post('/', async (req, res) => {
-    if(req.files === null) return res.status(400).json({msg: "No File Uploaded"});
+    console.log(req);
+    if(!req.files) return res.status(400).json({msg: "No File Uploaded"});
     const name = req.body.name;
     const rent = parseInt(req.body.rent);
     const size = req.body.size;
@@ -49,6 +52,51 @@ router.post('/', async (req, res) => {
             try {
                 await Cars.create({name: name, rent: rent, size: size, img: url});
                 res.status(201).json({msg: "Cars Created Successfuly"});
+            } catch (error) {
+                console.log(error.message);
+            }
+        })
+    }
+    else{
+        res.status(400).json({error: valid});
+    }
+    
+});
+
+//POST by view
+router.post('/postCar', async (req, res) => {
+    console.log(req);
+    if(!req.files) return res.status(400).json({msg: "No File Uploaded"});
+    const name = req.body.name;
+    const rent = parseInt(req.body.rent);
+    const size = req.body.size;
+    const file = req.files.img;
+    const fileSize = file.size
+    const ext = path.extname(file.name);
+    const fileName = file.md5 + ext;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    const allowedType = ['.png','.jpg','.jpeg'];
+
+    const schema = {
+        name: {type: "string", min: 3, max: 50},
+        rent: {type: "number", positive: true, integer: true, min: 1886, max: 2022},
+        size: {type: "string", min: 3, max: 50},
+        img: {type: "string", min: 3, max: 255}
+    };
+    const validate = v.compile(schema);
+    const valid = validate({name: name, rent: rent, size: size, img: url});
+
+    if(valid){
+        if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
+        if(fileSize > 2000000) return res.status(422).json({msg: "Image must be less than 2 MB"});
+
+        file.mv(`./public/images/${fileName}`, async(err)=>{
+            if(err) return res.status(500).json({msg: err.message});
+            try {
+                await Cars.create({name: name, rent: rent, size: size, img: url});
+                res.redirect('/saved')
+                // const cars = await Cars.findAll({order: [['name', 'ASC']]});
+                // return res.render('pages/index', { cars: cars, filter: "all", save: 'success' });
             } catch (error) {
                 console.log(error.message);
             }
@@ -104,7 +152,7 @@ router.put('/:id', async (req, res) => {
     if(valid){
         if(req.files){
             if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
-            if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
+            if(fileSize > 2000000) return res.status(422).json({msg: "Image must be less than 2 MB"});
 
             file.mv(`./public/images/${fileName}`, async(err)=>{
                 if(err) return res.status(500).json({msg: err.message});
@@ -130,14 +178,94 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+//PUT BY VIEW
+router.post('/updateCar/:id', async (req, res) => {
+    const car = await Cars.findByPk(req.params.id);
+    if(!car){
+        return res.status(404).json({msg: "No Data Found"});
+    }
+
+    let url;
+    let name;
+    let rent;
+    let size;
+    let fileName;
+    let ext = '';
+    let fileSize;
+    let file;
+    let allowedType = ['.png','.jpg','.jpeg'];
+
+    if(req.files){
+        file = req.files.img;
+        fileSize = file.size
+        ext = path.extname(file.name);
+        fileName = file.md5 + ext;
+        url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+    }
+    else{
+        url = car.img;
+    }
+
+    (req.body.name) ? name = req.body.name : name = car.name;
+    (req.body.rent) ? rent = parseInt(req.body.rent) : rent = car.rent;
+    (req.body.size) ? size = req.body.size : size = car.size;
+        
+    const schema = {
+        name: {type: "string", min: 3, max: 50},
+        year: {type: "number", positive: true, integer: true, min: 1886, max: 2022},
+        size: {type: "string", min: 3, max: 50},
+        img: {type: "string", min: 3, max: 255}
+    };
+    const validate = v.compile(schema);
+    const valid = validate({name: name, rent: rent, size: size, img: url});
+
+    if(valid){
+        if(req.files){
+            if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
+            if(fileSize > 2000000) return res.status(422).json({msg: "Image must be less than 2 MB"});
+
+            file.mv(`./public/images/${fileName}`, async(err)=>{
+                if(err) return res.status(500).json({msg: err.message});
+                try {
+                    await Cars.update({name: name, rent: rent, size: size, img: url}, {where: {id: req.params.id}});
+                    res.redirect('/saved');
+                } catch (error) {
+                    console.log(error.message);
+                }
+            })
+        }
+        else{
+            try {
+                await Cars.update({name: name, rent: rent, size: size, img: url}, {where: {id: req.params.id}});
+                res.redirect('/saved');
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    }
+    else{
+        res.status(400).json({error: valid});
+    }
+});
+
 //DELETE
 router.delete('/:id', async (req, res) => {
     const car = await Cars.findByPk(req.params.id);
     const fileName = car.img.split("/").pop();
     const filepath = `./public/images/${fileName}`;
-        fs.unlinkSync(filepath);
+    // fs.unlinkSync(filepath);
     const msg = await Cars.destroy({where: {id: req.params.id}});
     res.json(msg);
+});
+
+//DELETE
+router.get('/deleteCar/:id', async (req, res) => {
+    const car = await Cars.findByPk(req.params.id);
+    const fileName = car.img.split("/").pop();
+    const filepath = `./public/images/${fileName}`;
+    // fs.unlinkSync(filepath);
+    const msg = await Cars.destroy({where: {id: req.params.id}});
+    res.redirect("/");
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,32 @@ const {Cars} = require('../models');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
     const cars = await Cars.findAll({order: [['name', 'ASC']]});
-    console.log('this is cars', cars);
-    res.render('pages/index', { cars: cars});
+    res.render('pages/index', { cars: cars, filter: "all", save:""});
+});
+
+/* GET home page. saved */
+router.get('/saved', async function(req, res, next) {
+    const cars = await Cars.findAll({order: [['name', 'ASC']]});
+    res.render('pages/index', { cars: cars, filter: "all", save:"success"});
+});
+
+//filter
+/* GET Small */
+router.get('/small', async function(req, res, next) {
+    const cars = await Cars.findAll({order: [['name', 'ASC']], where: {size: 'Small'}});
+    res.render('pages/index', { cars: cars, filter: "small",  save:""});
+});
+
+/* GET Medium */
+router.get('/medium', async function(req, res, next) {
+    const cars = await Cars.findAll({order: [['name', 'ASC']], where: {size: 'Medium'}});
+    res.render('pages/index', { cars: cars, filter: "medium",  save:""});
+});
+
+/* GET Large */
+router.get('/large', async function(req, res, next) {
+    const cars = await Cars.findAll({order: [['name', 'ASC']], where: {size: 'Large'}});
+    res.render('pages/index', { cars: cars, filter: "large",  save:""});
 });
 
 /* GET add page. */
@@ -15,8 +39,13 @@ router.get('/add', function(req, res, next) {
 });
 
 /* GET update page. */
-router.get('/update', function(req, res, next) {
-    res.render('pages/update-page', { title: 'Express' });
+router.get('/update/:id', async function(req, res, next) {
+    const car = await Cars.findByPk(req.params.id);
+    // console.log(car)
+    if(!car){
+        return res.status(404).json({msg: "No Data Found"});
+    }
+    res.render('pages/update-page', { Cars: car });
 });
 
 module.exports = router;
